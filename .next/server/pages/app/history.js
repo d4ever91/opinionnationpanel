29d7f0a35e0248ee343bc6daa0a436fe2464b82d
d4ever91 <daljitsingh101@gmail.com>
2024-04-02 17:56:09 +0530

    sadf

---
+++ .next/server/pages/app/history.js
@@ -0,0 +1,536 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 602;
+exports.ids = [602];
+exports.modules = {
+
+/***/ 6742:
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.a(module, async (__webpack_handle_async_dependencies__, __webpack_async_result__) => { try {
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "SurveyList": () => (/* binding */ SurveyList),
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_dynamic__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(5152);
+/* harmony import */ var next_dynamic__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_dynamic__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _components_Route_WithAuth__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(7639);
+/* harmony import */ var _layouts_Seo__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(5500);
+/* harmony import */ var _components_App_Table_TableLayout__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(6018);
+/* harmony import */ var _chakra_ui_react__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(8930);
+/* harmony import */ var _chakra_ui_react__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_6__);
+/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(6022);
+/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_7___default = /*#__PURE__*/__webpack_require__.n(react_redux__WEBPACK_IMPORTED_MODULE_7__);
+/* harmony import */ var _lib_action_user__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(4702);
+/* harmony import */ var _components_App_Header_PageHeader__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(0);
+/* harmony import */ var _iconify_react__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(5566);
+/* harmony import */ var _components_Common_SpinnerLayout__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(4607);
+/* harmony import */ var _components_Common_DialogLayout__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(1961);
+/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_13__ = __webpack_require__(1853);
+/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_13___default = /*#__PURE__*/__webpack_require__.n(next_router__WEBPACK_IMPORTED_MODULE_13__);
+/* harmony import */ var _lib_action_survey__WEBPACK_IMPORTED_MODULE_14__ = __webpack_require__(2821);
+/* harmony import */ var _lib_constants_survey__WEBPACK_IMPORTED_MODULE_15__ = __webpack_require__(4703);
+var __webpack_async_dependencies__ = __webpack_handle_async_dependencies__([_components_Route_WithAuth__WEBPACK_IMPORTED_MODULE_3__, _components_App_Table_TableLayout__WEBPACK_IMPORTED_MODULE_5__, _lib_action_user__WEBPACK_IMPORTED_MODULE_8__, _components_App_Header_PageHeader__WEBPACK_IMPORTED_MODULE_9__, _iconify_react__WEBPACK_IMPORTED_MODULE_10__, _lib_action_survey__WEBPACK_IMPORTED_MODULE_14__]);
+([_components_Route_WithAuth__WEBPACK_IMPORTED_MODULE_3__, _components_App_Table_TableLayout__WEBPACK_IMPORTED_MODULE_5__, _lib_action_user__WEBPACK_IMPORTED_MODULE_8__, _components_App_Header_PageHeader__WEBPACK_IMPORTED_MODULE_9__, _iconify_react__WEBPACK_IMPORTED_MODULE_10__, _lib_action_survey__WEBPACK_IMPORTED_MODULE_14__] = __webpack_async_dependencies__.then ? (await __webpack_async_dependencies__)() : __webpack_async_dependencies__);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const SurveyList = ()=>{
+    const router = (0,next_router__WEBPACK_IMPORTED_MODULE_13__.useRouter)();
+    const { 0: filters , 1: setFilters  } = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)({});
+    const dispatch = (0,react_redux__WEBPACK_IMPORTED_MODULE_7__.useDispatch)();
+    const { isLoading , surveys , users , statuses , countries , meta  } = (0,react_redux__WEBPACK_IMPORTED_MODULE_7__.useSelector)((state)=>state.survey);
+    (0,react__WEBPACK_IMPORTED_MODULE_1__.useEffect)(()=>{
+        if (filters.search) {
+            const timer = setTimeout(()=>{
+                getSurveysList(filters);
+            }, 500);
+            return ()=>clearTimeout(timer);
+        } else {
+            getSurveysList(filters);
+        }
+    }, [
+        filters
+    ]);
+    const getSurveysList = async (filters)=>{
+        await dispatch((0,_lib_action_survey__WEBPACK_IMPORTED_MODULE_14__/* .getSurveys */ .x6)(filters));
+    };
+    const changeAction = async (key, value)=>{
+        if (value && value.label) {
+            var value = value.value;
+        }
+        if (!value) delete filters[key];
+        else filters[key] = value;
+        setFilters({
+            ...filters
+        });
+    };
+    const myLoader = ({ src  })=>{
+        return `https://minimals.cc/assets/icons/apps/ic_chrome.svg`;
+    };
+    const formInputs = [
+        {
+            key: "search",
+            label: "Search...",
+            type: "text",
+            validationType: "string",
+            size: "md"
+        },
+        {
+            key: "user",
+            label: "Project Manager",
+            type: "select",
+            validationType: "string",
+            isClearable: true,
+            options: users && users.map((option)=>({
+                    ...option,
+                    label: option.email + "(" + option.count + ")",
+                    value: option._id
+                })),
+            size: "md"
+        },
+        {
+            key: "country",
+            label: "Country",
+            type: "select",
+            validationType: "string",
+            isClearable: true,
+            options: countries && countries.map((option)=>({
+                    ...option,
+                    label: option.name + "(" + option.count + ")",
+                    value: option._id
+                })),
+            size: "md"
+        },
+        {
+            key: "status",
+            label: "Status",
+            type: "select",
+            isClearable: true,
+            validationType: "string",
+            options: statuses && statuses.map((option)=>({
+                    ...option,
+                    label: option.label + "(" + option.count + ")",
+                    value: option.value
+                })),
+            size: "md"
+        }, 
+    ];
+    const columns = [
+        {
+            key: "status",
+            title: "Status",
+            width: 200,
+            render: (_, { status  })=>{
+                return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_6__.Box, {
+                    display: "flex",
+                    children: [
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_iconify_react__WEBPACK_IMPORTED_MODULE_10__.Icon, {
+                            icon: "ci:dot-04-l",
+                            color: _lib_constants_survey__WEBPACK_IMPORTED_MODULE_15__/* .SurveyStatus.find */ ._.find((surveystatus)=>surveystatus.value == status).color,
+                            fontSize: 24
+                        }),
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_6__.Text, {
+                            children: _lib_constants_survey__WEBPACK_IMPORTED_MODULE_15__/* .SurveyStatus.find */ ._.find((surveystatus)=>surveystatus.value == status).label
+                        })
+                    ]
+                });
+            }
+        },
+        {
+            key: "surveyName",
+            title: "Survey Name",
+            width: 200
+        },
+        {
+            key: "category",
+            title: "Category",
+            width: 200
+        },
+        {
+            key: "reward",
+            title: "Reward",
+            width: 200
+        },
+        {
+            key: "_id",
+            title: "Date",
+            width: 200,
+            render: (_, { _id , surveyStatus  })=>{
+                const { isOpen , onOpen , onClose  } = (0,_chakra_ui_react__WEBPACK_IMPORTED_MODULE_6__.useDisclosure)();
+                const onSubmit = async ()=>{
+                    await dispatch((0,_lib_action_user__WEBPACK_IMPORTED_MODULE_8__/* .removeUser */ .kX)(_id));
+                };
+                return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_6__.Flex, {
+                    children: [
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_Common_DialogLayout__WEBPACK_IMPORTED_MODULE_12__/* ["default"] */ .Z, {
+                            isOpen: isOpen,
+                            onClose: onClose,
+                            onSubmit: onSubmit
+                        }),
+                        surveyStatus == 1 && /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_6__.Link, {
+                            marginRight: "3",
+                            "aria-label": "Edit",
+                            onClick: ()=>router.push(`/app/survey/${_id}/edit`),
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_iconify_react__WEBPACK_IMPORTED_MODULE_10__.Icon, {
+                                fontSize: 24,
+                                icon: "carbon:play-filled-alt",
+                                color: "#cad6f3"
+                            })
+                        }),
+                        surveyStatus == 2 && /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_6__.Link, {
+                            marginRight: "3",
+                            "aria-label": "Edit",
+                            onClick: ()=>router.push(`/app/survey/${_id}/edit`),
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_iconify_react__WEBPACK_IMPORTED_MODULE_10__.Icon, {
+                                fontSize: 24,
+                                icon: "material-symbols:pause",
+                                color: "#cad6f3"
+                            })
+                        }),
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_6__.Link, {
+                            marginRight: "3",
+                            "aria-label": "Edit",
+                            onClick: onOpen,
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_iconify_react__WEBPACK_IMPORTED_MODULE_10__.Icon, {
+                                fontSize: 24,
+                                icon: "fa-regular:clone",
+                                color: "#cad6f3"
+                            })
+                        }),
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_6__.Link, {
+                            marginRight: "3",
+                            "aria-label": "Delete",
+                            onClick: onOpen,
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_iconify_react__WEBPACK_IMPORTED_MODULE_10__.Icon, {
+                                fontSize: 24,
+                                icon: "ion:stats-chart",
+                                color: "#cad6f3"
+                            })
+                        })
+                    ]
+                }, _id);
+            }
+        }, 
+    ];
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_6__.Box, {
+        children: [
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_layouts_Seo__WEBPACK_IMPORTED_MODULE_4__/* .Seo */ .p, {
+                title: "Surveys"
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_App_Header_PageHeader__WEBPACK_IMPORTED_MODULE_9__/* ["default"] */ .Z, {
+                title: "Surveys"
+            }),
+            isLoading ? /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_Common_SpinnerLayout__WEBPACK_IMPORTED_MODULE_11__/* ["default"] */ .Z, {}) : /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_App_Table_TableLayout__WEBPACK_IMPORTED_MODULE_5__/* .TableLayout */ .d, {
+                data: surveys,
+                meta: meta,
+                columns: columns
+            })
+        ]
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (next_dynamic__WEBPACK_IMPORTED_MODULE_2___default()(()=>Promise.resolve((0,_components_Route_WithAuth__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .Z)(SurveyList)), {
+    ssr: false
+}));
+
+__webpack_async_result__();
+} catch(e) { __webpack_async_result__(e); } });
+
+/***/ }),
+
+/***/ 8930:
+/***/ ((module) => {
+
+module.exports = require("@chakra-ui/react");
+
+/***/ }),
+
+/***/ 5184:
+/***/ ((module) => {
+
+module.exports = require("@reduxjs/toolkit");
+
+/***/ }),
+
+/***/ 6517:
+/***/ ((module) => {
+
+module.exports = require("lodash");
+
+/***/ }),
+
+/***/ 1649:
+/***/ ((module) => {
+
+module.exports = require("next-auth/react");
+
+/***/ }),
+
+/***/ 3280:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/app-router-context.js");
+
+/***/ }),
+
+/***/ 2796:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/head-manager-context.js");
+
+/***/ }),
+
+/***/ 3539:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/i18n/detect-domain-locale.js");
+
+/***/ }),
+
+/***/ 4014:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/i18n/normalize-locale-path.js");
+
+/***/ }),
+
+/***/ 8524:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/is-plain-object.js");
+
+/***/ }),
+
+/***/ 5832:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/loadable.js");
+
+/***/ }),
+
+/***/ 8020:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/mitt.js");
+
+/***/ }),
+
+/***/ 4406:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/page-path/denormalize-page-path.js");
+
+/***/ }),
+
+/***/ 4964:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router-context.js");
+
+/***/ }),
+
+/***/ 3431:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/add-locale.js");
+
+/***/ }),
+
+/***/ 1751:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/add-path-prefix.js");
+
+/***/ }),
+
+/***/ 6220:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/compare-states.js");
+
+/***/ }),
+
+/***/ 299:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/format-next-pathname-info.js");
+
+/***/ }),
+
+/***/ 3938:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/format-url.js");
+
+/***/ }),
+
+/***/ 9565:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/get-asset-path-from-route.js");
+
+/***/ }),
+
+/***/ 5789:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/get-next-pathname-info.js");
+
+/***/ }),
+
+/***/ 1428:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/is-dynamic.js");
+
+/***/ }),
+
+/***/ 8854:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/parse-path.js");
+
+/***/ }),
+
+/***/ 1292:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/parse-relative-url.js");
+
+/***/ }),
+
+/***/ 4567:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/path-has-prefix.js");
+
+/***/ }),
+
+/***/ 979:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/querystring.js");
+
+/***/ }),
+
+/***/ 3297:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/remove-trailing-slash.js");
+
+/***/ }),
+
+/***/ 6052:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/resolve-rewrites.js");
+
+/***/ }),
+
+/***/ 4226:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/route-matcher.js");
+
+/***/ }),
+
+/***/ 5052:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/route-regex.js");
+
+/***/ }),
+
+/***/ 9232:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/utils.js");
+
+/***/ }),
+
+/***/ 968:
+/***/ ((module) => {
+
+module.exports = require("next/head");
+
+/***/ }),
+
+/***/ 1853:
+/***/ ((module) => {
+
+module.exports = require("next/router");
+
+/***/ }),
+
+/***/ 6689:
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 6022:
+/***/ ((module) => {
+
+module.exports = require("react-redux");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ }),
+
+/***/ 5566:
+/***/ ((module) => {
+
+module.exports = import("@iconify/react");;
+
+/***/ }),
+
+/***/ 9648:
+/***/ ((module) => {
+
+module.exports = import("axios");;
+
+/***/ }),
+
+/***/ 6201:
+/***/ ((module) => {
+
+module.exports = import("react-hot-toast");;
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [2952,7142,5500,8137,921,183,6452,4702,6871,622,2821], () => (__webpack_exec__(6742)));
+module.exports = __webpack_exports__;
+
+})();
